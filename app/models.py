97d021fbc2a90b33ecde236b15f261c4d50e0291
Oleg Dudkin <dudkin.on@gmail.com>
2018-06-07 04:49:49 -0500

    feat: Add ProductArea model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Date, DateTime
+from sqlalchemy import create_engine, Date
 from sqlalchemy.orm import sessionmaker
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import BadSignature, SignatureExpired
@@ -107,6 +107,13 @@ class User(Base):
             'status': self.status,
             'role': self.role
         }
+
+
+class ProductArea(Base):
+
+    __tablename__ = 'product_area'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60))
 
 
 # create an engine
