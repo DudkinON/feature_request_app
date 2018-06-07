54a4092cc5d980fb251c99371687a5154ac3ce66
Oleg Dudkin <dudkin.on@gmail.com>
2018-06-07 04:54:34 -0500

    feat: Add Client model

--- app/models.py
+++ app/models.py
@@ -134,6 +134,13 @@ class Priority(Base):
     id = Column('id', Integer, primary_key=True)
 
 
+class Client(Base):
+
+    __tablename__ = 'client'
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String(50))
+
+
 # create an engine
 if POSTGRES:
     engine = create_engine(CONNECT_SETTINGS)
