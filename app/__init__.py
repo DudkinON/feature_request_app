552cd9dbefdd629929cd525511764a7c0fa75c83
Oleg Dudkin <dudkin.on@gmail.com>
2018-06-09 20:25:07 -0500

    feat: Add check_request decorator function

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ from bleach import clean
 
 from data_provider import *
 from settings import SECRETS_DIR, app_host, app_port, app_debug
-from resource import get_unique_str, is_index
+from resource import get_unique_str, is_index, convert_date
 from secrets import keys
 
 # define global variables
@@ -24,10 +24,10 @@ auth = HTTPBasicAuth()
 
 def login_required(f):
     """
-    Checking the user is logged in
+    Checking to see if the user is logged in
 
     :param f: function
-    :return: function if user legged in else send error in JSON format
+    :return: function if user logged in else send error in JSON format
     """
 
     @wraps(f)
@@ -48,7 +48,7 @@ def csrf_protection(f):
     CSRF protection
 
     :param f: function
-    :return: function if csrf tokens are match else send error in JSON format
+    :return: function if csrf tokens are matched else send error in JSON format
     """
 
     @wraps(f)
@@ -88,6 +88,69 @@ def verify_password(_login, password):
                 return False
     g.user = user
     return True
+
+
+def check_request(f):
+
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+
+        # get JSON data
+        data = request.get_json()
+
+        user_request = dict()
+
+
+
+        # validate fields
+        field_validation("title")
+        field_validation("description")
+        field_validation("date")
+
+        # define fields
+        user_request["title"] = clean(data.get("title"))
+        user_request["description"] = clean(data.get("description"))
+        user_request["client"] = None
+        user_request["product_area"] = None
+        user_request["client_priority"] = None
+        user_request["target_date"] = convert_date(data.get("target_date"))
+
+        if user_request["target_date"] is None:
+            return jsonify({'error': "The date has the wrong format"}), 200
+
+        client = data.get("client")
+        if client and is_index(client.get("client_priority")):
+            user_request['client_priority'] = int(
+                client.get("client_priority"))
+        else:
+            return jsonify(
+                {'error': "Client priority has to be an integer"}), 200
+
+        if client and is_index(client.get("id")):
+            user_request["client"] = int(client.get("id"))
+        else:
+            return jsonify({'error': "Client id has to be an integer"})
+
+        product_area = data.get("product_area")
+
+        if product_area and is_index(product_area.get('id')):
+            user_request["product_area"] = int(product_area.get('id'))
+        else:
+            return jsonify(
+                {'error': "Product area id has to be an integer"}), 200
+
+        if not client_exist(user_request["client"]):
+            return jsonify({'error': "The client is not found"}), 200
+
+        if not product_area_exist(user_request["product_area"]):
+            return jsonify({'error': "The product area is not found"}), 200
+
+        g.user_request = user_request
+
+        return f(*args, **kwargs)
+
+    return decorated_function
 
 
 if __name__ == '__main__':
