0f831d103f3c3ba3819e3d60f34b195beafac648
Oleg Dudkin <dudkin.on@gmail.com>
2018-06-10 21:30:54 -0500

    docs: Add a docstring to get_unique_str function

--- app/resource.py
+++ app/resource.py
@@ -6,5 +6,10 @@ from string import ascii_uppercase as uppercase, digits
 
 
 def get_unique_str(amount):
+    """
+    Return a unique string
 
+    :param amount: (int)
+    :return string:
+    """
     return ''.join(choice(uppercase + digits) for x in xrange(amount))
