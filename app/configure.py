7e00e7088f0566be5a6f23e058c4527c3dcb6b34
Oleg Dudkin <dudkin.on@gmail.com>
2018-06-11 14:34:02 -0500

    feat: Add Storage class

--- app/configure.py
+++ app/configure.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from os import path
 from settings import SAME_THREAD, DB_FILE_NAME, CONNECT_SETTINGS, POSTGRES
+from secrets.keys import secret_key
 
-SQLite_CONF = 'sqlite:///%s' % DB_FILE_NAME
+
+cur_dir = path.dirname(path.abspath(__file__))
+SQLite = 'sqlite:///%s%s'
 
 if not SAME_THREAD:
-    SQLite_CONF += '?check_same_thread=False'
+    cst = '?check_same_thread=False'
 else:
-    SQLite_CONF += '?check_same_thread=True'
+    cst = '?check_same_thread=True'
 
 if POSTGRES:
     DB_SETTINGS = CONNECT_SETTINGS
 else:
-    DB_SETTINGS = SQLite_CONF
+    DB_SETTINGS = SQLite
+
+
+class SQLiteConfig(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = secret_key
+    SQLALCHEMY_DATABASE_URI = SQLite % (path.join(cur_dir, DB_FILE_NAME), cst)
+    SQLALCHEMY_ECHO = False
+
+
+class SQLiteConfigTest(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = secret_key
+    SQLALCHEMY_DATABASE_URI = SQLite % (path.join(cur_dir, 'test.db'), cst)
+    SQLALCHEMY_ECHO = False
+
+
+class PostgreSQLConfig(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = secret_key
+    PostgreSQL_DATABASE = CONNECT_SETTINGS
+    SQLALCHEMY_ECHO = False
--- app/tests.py
+++ app/tests.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase
+from settings import HOST, CREDENTIALS
+from requests import Session
+from re import search
+from json import dumps
+
+
+req_session = Session()
+
+
+class Storage(object):
+    pass
