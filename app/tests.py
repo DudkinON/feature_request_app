3f2894263fb8c204d97d3126899eecdfe4e4a5df
Oleg Dudkin <dudkin.on@gmail.com>
2018-06-11 14:55:58 -0500

    feat: Add a docstring to test_front_end method of TestApp class

--- app/tests.py
+++ app/tests.py
@@ -104,7 +104,14 @@ class TestApp(TestCase):
         self.headers = {'content-type': 'application/json'}
 
     def test_front_end(self):
+        """
+        Test for front_end function. Test status code
+        and content type. Get csrf token and cookies
+        from the request and save them, for following
+        test function.
 
+        :return void:
+        """
         r = req_session.get(HOST)
         content_type = 'text/html; charset=utf-8'
         pattern = r'\sdata-csrf-token=\"([A-Z\d]{36})\"'
