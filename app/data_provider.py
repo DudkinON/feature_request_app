e4be275f43df590fd395142dd59550dcc8b27018
Oleg Dudkin <dudkin.on@gmail.com>
2018-06-13 10:51:39 -0500

    feat: Add get_user_by_id function

--- app/data_provider.py
+++ app/data_provider.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from models import User, ProductArea, Client, Request, session
+
+query = session.query
+
+
+def get_user_by_id(uid):
+
+    return query(User).filter_by(id=uid).first() or None
